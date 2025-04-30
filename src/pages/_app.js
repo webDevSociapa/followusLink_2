b26f9537b81a0819e8c1b755b2b35e1f4bf1a769
webDevSociapa <webdev@sociapa.com>
2025-04-30 16:46:24 +0530

    ff

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,11 +5,12 @@ import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "../redux/store";
 import "../styles/globals.css";
 
+
 function AuthGuard({ children }) {
   const router = useRouter();
   const authToken = useSelector((state) => state.auth?.authToken);
   const isAuthenticated = !!authToken; // Converts truthy/falsy value into boolean
-  
+
 
   useEffect(() => {
     const protectedRoutes = ["/admin", "/template"]; // Add all protected routes here
